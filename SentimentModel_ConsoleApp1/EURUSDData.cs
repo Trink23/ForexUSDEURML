c0e936e4dc09075882ff11a373100295c6207194
guiller anguita <guiller3334@outlook.com>
2022-07-18 20:59:39 +0100

    Now we are using timeseries argument.

--- SentimentModel_ConsoleApp1/EURUSDData.cs
+++ SentimentModel_ConsoleApp1/EURUSDData.cs
@@ -9,40 +9,40 @@ namespace SentimentModel_ConsoleApp1
 {
     internal class EURUSDData
     {
-        [ColumnName(@"Date")]
+        [LoadColumn(0), ColumnName(@"Date")]
         public string Date { get; set; }
 
-        [ColumnName(@"Time")]
+        [LoadColumn(1), ColumnName(@"Time")]
         public string Time { get; set; }
 
-        [ColumnName(@"BO")]
+        [LoadColumn(2), ColumnName(@"BO")]
         public float BO { get; set; }
 
-        [ColumnName(@"BH")]
+        [LoadColumn(3), ColumnName(@"BH")]
         public float BH { get; set; }
 
-        [ColumnName(@"BL")]
+        [LoadColumn(4), ColumnName(@"BL")]
         public float BL { get; set; }
 
-        [ColumnName(@"BC")]
+        [LoadColumn(5), ColumnName(@"BC")]
         public float BC { get; set; }
 
-        [ColumnName(@"BCh")]
+        [LoadColumn(6), ColumnName(@"BCh")]
         public float BCh { get; set; }
 
-        [ColumnName(@"AO")]
+        [LoadColumn(7), ColumnName(@"AO")]
         public float AO { get; set; }
 
-        [ColumnName(@"AH")]
+        [LoadColumn(8), ColumnName(@"AH")]
         public float AH { get; set; }
 
-        [ColumnName(@"AL")]
+        [LoadColumn(9), ColumnName(@"AL")]
         public float AL { get; set; }
 
-        [ColumnName(@"AC")]
+        [LoadColumn(10), ColumnName(@"AC")]
         public float AC { get; set; }
 
-        [ColumnName(@"ACh")]
+        [LoadColumn(11), ColumnName(@"ACh")]
         public float ACh { get; set; }
     }
 }
---
+++ SentimentModel_ConsoleApp1/EURUSDForecast.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentModel_ConsoleApp1
+{
+    internal class EURUSDForecast
+    {
+        public float[] Forecast { get; set; }
+    }
+}
